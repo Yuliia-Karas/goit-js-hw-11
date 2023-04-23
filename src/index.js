b3a4d3d79482bc4d535735aa5a4faf73c6a36203
Yuliia Karas <iuliiakaras@gmail.com>
2023-04-23 23:06:38 +0200

    make constants

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import './sass/index.scss'
+
+import axios from 'axios';
+import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const searchForm = document.querySelector('#search-form');
+const photoGallery = document.querySelector('.gallery');
+const loadMoreBtn = document.querySelector('.load-more');
+
+const KEY = '35692508-ed297a5167f9400201d2ec2b1';
+let page = 1;
+const perPage = 40;
+const imageType = 'photo';
+const imageOrientation = 'horizontal';
+const safeSearch = 'true';
+
+const URL = 'https://pixabay.com/api/?key=${KEY}&q=${searchImages}&image_type=${imageType}&orientation=${imageOrientation}&safesearch=${safeSearch}&per_page=${perPage}&page=${page}'
+
