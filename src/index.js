d22989dc36cbe9e23ae6e20d8913b77d1f472ed4
Yuliia Karas <iuliiakaras@gmail.com>
2023-04-25 19:26:31 +0200

    make request

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './sass/index.scss';
 
-import axios from 'axios';
+
 import Notiflix from 'notiflix';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
@@ -9,7 +9,7 @@ const searchForm = document.querySelector('#search-form');
 const photoGallery = document.querySelector('.gallery');
 const loadMoreBtn = document.querySelector('.load-more');
 
-const KEY = '35692508-ed297a5167f9400201d2ec2b1';
+
 let page = 1;
 const perPage = 40;
 const imageType = 'photo';
@@ -23,7 +23,7 @@ async function getResponse(searchImages) {
        'https://pixabay.com/api/?key=${KEY}&q=${searchImages}&image_type=${imageType}&orientation=${imageOrientation}&safesearch=${safeSearch}&per_page=${perPage}&page=${page}';
  
     const response = await axios.get(URL);
-    if (response.status === 404) {
+    if (!response.ok) {
       return [];
       }
           return await response.data;
---
+++ src/pixabay-gallery.js
@@ -0,0 +1,52 @@
+import axios from 'axios';
+
+const KEY = '35692508-ed297a5167f9400201d2ec2b1';
+const URL =
+  'https://pixabay.com/api/?key=${KEY}&q=${searchImages}&image_type=${imageType}&orientation=${imageOrientation}&safesearch=${safeSearch}&per_page=${perPage}&page=${page}';
+
+export default class PixabayGallery {
+  constructor() {
+    this.name = '';
+    this.page = 1;
+    this.perPage = 40;
+    this.totalPage = 0;
+  }
+
+  async getResponse() {
+    try {
+      const response = await axios.get(`${URL}`, {
+        params: {
+          key: `${KEY}`,
+          q: `${this.name}`,
+          imageType: 'photo',
+          imageOrientation: 'horizontal',
+          safeSearch: 'true',
+          perPage: `${this.perPage}`,
+          page: `${this.page}`,
+        },
+      });
+      this.incrementPage();
+      return response;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  incrementPage() {
+    this.page += 1;
+  }
+  resetPage() {
+    this.page = 1;
+  }
+
+  get getName() {
+    return this.name;
+  }
+
+  set setName(newName) {
+    this.name = newName;
+  }
+
+  set setTotalPage(newTotalPages) {
+    this.totalPages = newTotalPages;
+  }
+}
