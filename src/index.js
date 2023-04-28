88b2137a6f83a88b776ffc41ff1afecd7f6e6277
Yuliia Karas <iuliiakaras@gmail.com>
2023-04-28 08:53:21 +0200

    update index.js

--- src/index.js
+++ src/index.js
@@ -9,28 +9,24 @@ const searchForm = document.querySelector('.search-form');
 const input = document.querySelector('input');
 const gallery = document.querySelector('.gallery');
 const endGallery = document.querySelector('.end');
-// const loadMoreBtn = document.querySelector('.load-more');
 
 let observer;
+smoothScroll();
 
 const searchImages = async e => {
   e.preventDefault();
   pixabayGallery.name = input.value.trim();
   pixabayGallery.resetPage();
 
-  // const observer = new IntersectionObserver(onEntry, observerOptions);
-
   observer = new IntersectionObserver(onEntry, observerOptions);
 
-
   if (pixabayGallery.name === '') {
-    return Notify.failure('What you want to find?');
+    return Notify.failure('What do you want to find?');
   }
 
   try {
     const response = await pixabayGallery.getResponse();
     gallery.innerHTML = '';
-    smoothScroll();
 
     pixabayGallery.totalPages = Math.ceil(
       response.data?.totalHits / response.config.params.per_page
@@ -41,18 +37,21 @@ const searchImages = async e => {
       Notify.failure(
         'Sorry, there are no images matching your search query. Please try again.'
       );
-    } else if (
-      pixabayGallery.page > pixabayGallery.totalPages ||
-      response.data.totalHits < pixabayGallery.perPage
-    ) {
-      observer.unobserve(endGallery);
-      Notify.info("We're sorry, but you've reached the end of search results.");
     } else {
       Notify.success(`Hooray! We found ${response.data.totalHits} images.`);
       observer.observe(endGallery);
-    }
+      if (
+        pixabayGallery.page - 1 > pixabayGallery.totalPages ||
+        response.data.totalHits <= pixabayGallery.perPage
+      ) {
+        observer.unobserve(endGallery);
+        Notify.info(
+          "We're sorry, but you've reached the end of search results."
+        );
+      }
 
-    insertContent(response.data.hits);
+      insertContent(response.data.hits);
+    }
   } catch (error) {
     console.log(error);
   }
@@ -68,22 +67,21 @@ function smoothScroll() {
 
 const onEntry = entries => {
   entries.forEach(entry => {
-    if (entry.isIntersecting && pixabayGallery.name !== '') {
+    if (
+      entry.isIntersecting &&
+      pixabayGallery.name !== '' &&
+      pixabayGallery.page - 1 < pixabayGallery.totalPages
+    ) {
       pixabayGallery
         .getResponse()
         .then(response => {
-                if (
-            pixabayGallery.page > pixabayGallery.totalPages ||
-            response.data.totalHits < pixabayGallery.perPage
-          ) {
+          if (pixabayGallery.page - 1 >= pixabayGallery.totalPages) {
             observer.unobserve(endGallery);
             Notify.info(
               "We're sorry, but you've reached the end of search results."
             );
-          } else {
-            insertContent(response.data.hits);
-          } 
-          
+          }
+          insertContent(response.data.hits);
         })
         .catch(error => console.log(error));
     }
@@ -91,7 +89,7 @@ const onEntry = entries => {
 };
 
 const observerOptions = {
-  rootMargin: '300px',
+  rootMargin: '500px',
 };
 
 searchForm.addEventListener('submit', searchImages);
